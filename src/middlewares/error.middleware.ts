26e80d739cc51c38054d2e36888bdc3d9eaabad3
Praise Jacob <iampraisejacob@gmail.com>
2025-10-11 20:18:30 +0530

    fix: change error handler login, add a more robust error map with support for custom function based handler instead of static object, add handler for MongoServerError E11000 (caused by duplicate key in a index

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,16 +4,32 @@ import { logger } from "@logger";
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-type KnownError = "CastError";
+type KnownError = "CastError" | "MongoServerError";
 interface IErrorResponse {
     code: number;
     message: string;
 }
 
-const errorMap: Record<KnownError, IErrorResponse> = {
+// Either a static response or a function that returns a response based on the error
+// function can return void if it doesn't want to handle the error
+type ErrorResponse =
+    | IErrorResponse
+    | { fn: (err: Error) => IErrorResponse | void };
+
+const errorMap: Record<KnownError, ErrorResponse> = {
     CastError: {
         code: StatusCodes.BAD_REQUEST,
         message: "Invalid ID format",
+    },
+    MongoServerError: {
+        fn: (err: Error) => {
+            if ("code" in err && err.code === 11000) {
+                return {
+                    code: StatusCodes.CONFLICT,
+                    message: "Duplicate key error",
+                };
+            }
+        },
     },
 };
 
@@ -24,27 +40,41 @@ export function errorHandler(
     _next: NextFunction,
 ): void {
     if (isKnownError(error.name)) {
-        const { code, message } = errorMap[error.name];
-        res.status(code).json({
-            error: error.name,
-            message,
-        });
+        const handler = errorMap[error.name];
+
+        // handler with fn
+        if ("fn" in handler) {
+            const result = handler.fn(error);
+            if (result) {
+                res.status(result.code).json({
+                    error: error.name,
+                    message: result.message,
+                });
+                return;
+            }
+        } else {
+            // static handler
+            res.status(handler.code).json({
+                error: error.name,
+                message: handler.message,
+            });
+        }
     } else if (error instanceof ServerError) {
         res.status(error.statusCode).json({
             error: error.name,
             message: error.message,
         });
-    } else {
-        logger.error(error, "Unhandled error");
-
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            error: error.name,
-            message:
-                ENV === "production"
-                    ? "Internal Server Error"
-                    : (error.message ?? "Internal Server Error"),
-        });
+        return;
     }
+
+    logger.error(error, "Unhandled error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: error.name,
+        message:
+            ENV === "production"
+                ? "Internal Server Error"
+                : (error.message ?? "Internal Server Error"),
+    });
 }
 
 function isKnownError(name: string): name is KnownError {
