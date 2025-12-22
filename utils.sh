ac16277c639da8d5d06aff6d05c4b5fd1f7097a5
Praise Jacob <iampraisejacob@gmail.com>
2025-12-23 02:00:25 +0530

    chore: add get method to utils script

--- utils.sh
+++ utils.sh
@@ -4,12 +4,21 @@ find-relative-imports() {
     fd . -t f -ets | xargs grep -nE 'import .* from "\.' | awk -F: '{ print $2 " " $1 }'
 }
 
-post() {
+get_cookies() {
     COOKIE_JAR=/tmp/cookie_jar.txt
     CSRF_TOKEN=$(curl -sLi -c $COOKIE_JAR "localhost:5173/api/csrf" | grep -Po '(?<=csrf_token=)[0-9a-zA-Z]+(?=;)')
+}
 
+post() {
+    get_cookies
     curl -sLi -b $COOKIE_JAR -c $COOKIE_JAR -X POST \
         -H "Content-Type: application/json" \
+        -H "X-CSRF-Token: $CSRF_TOKEN" "$@"
+}
+
+get() {
+    get_cookies
+    curl -sLi -b $COOKIE_JAR -c $COOKIE_JAR \
         -H "X-CSRF-Token: $CSRF_TOKEN" "$@"
 }
 
