4d35e002aa8a5e162f675e0d71a50ade2160a58e
Praise Jacob <iampraisejacob@gmail.com>
2026-02-20 22:39:22 +0530

    chore: initial monorepo structure

---
+++ apps/backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import { AuthSchema, type IAuth } from "@/models/auth.schema.js";
+import { type Document, type Model, model, Schema, type Types } from "mongoose";
+
+import { type IVault, VaultSchema } from "./vault.schema.js";
+
+export interface IUser extends Document {
+	_id: Types.ObjectId;
+	username: string;
+	email: string;
+	auth: IAuth;
+	vault: IVault;
+}
+
+const userSchema: Schema<IUser> = new Schema<IUser>({
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 1,
+		maxlength: 100,
+	},
+	email: {
+		type: String,
+		required: false,
+		unique: true,
+		trim: true,
+		match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+	},
+	auth: AuthSchema,
+	vault: VaultSchema,
+});
+
+export const User: Model<IUser> = model<IUser>("User", userSchema);
